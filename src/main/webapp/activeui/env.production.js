5ee0225d1211733ec2c77b4618602ac77829a128
Olivier Peyrusse <ope@activeviam.com>
2020-04-09 17:07:08 +0200

    Update ActiveUI to its latest version. (#14)

--- src/main/webapp/activeui/env.production.js
+++ src/main/webapp/activeui/env.production.js
@@ -1,9 +1,7 @@
 var baseUrl = window.location.href.split('/ui')[0];
 
 window.env = {
-  serverUrls: {
-    activePivot: baseUrl,
-    activeMonitor: baseUrl,
-    content: baseUrl
-  }
+    activePivotServerUrl: baseUrl,
+    activeMonitorServerUrl: baseUrl,
+    contentServerUrl: baseUrl
 };
