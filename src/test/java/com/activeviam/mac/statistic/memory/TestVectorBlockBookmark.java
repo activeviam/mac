5ae3d6212494960e1c01c0c3b9429963c7f011cb
leguenv <valentin-leguen@hotmail.fr>
2020-10-22 09:39:35 +0200

    update vector bookmark test

--- src/test/java/com/activeviam/mac/statistic/memory/TestVectorBlockBookmark.java
+++ src/test/java/com/activeviam/mac/statistic/memory/TestVectorBlockBookmark.java
@@ -1,178 +1,134 @@
-///*
-// * (C) ActiveViam 2020
-// * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
-// * property of ActiveViam. Any unauthorized use,
-// * reproduction or transfer of this material is strictly prohibited
-// */
-//
-//package com.activeviam.mac.statistic.memory;
-//
-//import com.activeviam.mac.cfg.impl.ManagerDescriptionConfig;
-//import com.activeviam.mac.statistic.memory.visitor.impl.FeedVisitor;
-//import com.activeviam.pivot.builders.StartBuilding;
-//import com.qfs.monitoring.offheap.MemoryStatisticsTestUtils;
-//import com.qfs.monitoring.offheap.MemoryStatisticsTestUtils.StatisticsSummary;
-//import com.qfs.monitoring.statistic.memory.IMemoryStatistic;
-//import com.qfs.pivot.monitoring.impl.MemoryAnalysisService;
-//import com.qfs.store.IDatastore;
-//import com.quartetfs.biz.pivot.IActivePivotManager;
-//import com.quartetfs.biz.pivot.IMultiVersionActivePivot;
-//import com.quartetfs.biz.pivot.dto.CellSetDTO;
-//import com.quartetfs.biz.pivot.query.impl.MDXQuery;
-//import com.quartetfs.fwk.AgentException;
-//import com.quartetfs.fwk.Registry;
-//import com.quartetfs.fwk.contributions.impl.ClasspathContributionProvider;
-//import com.quartetfs.fwk.impl.Pair;
-//import com.quartetfs.fwk.query.QueryException;
-//import java.nio.file.Path;
-//import java.util.stream.IntStream;
-//import org.assertj.core.api.Assertions;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//
-//public class TestVectorBlockBookmark extends ATestMemoryStatistic {
-//
-//  Pair<IDatastore, IActivePivotManager> monitoredApp;
-//  Pair<IDatastore, IActivePivotManager> monitoringApp;
-//
-//  StatisticsSummary summary;
-//
-//  public static final int ADDED_DATA_SIZE = 20;
-//  public static final int FIELD_SHARING_COUNT = 2;
-//
-//  @BeforeClass
-//  public static void setupRegistry() {
-//    Registry.setContributionProvider(new ClasspathContributionProvider());
-//  }
-//
-//  @Before
-//  public void setup() throws AgentException {
-//    monitoredApp = createMicroApplicationWithSharedVectorField();
-//
-//    monitoredApp.getLeft()
-//        .edit(tm -> IntStream.range(0, ADDED_DATA_SIZE)
-//            .forEach(i -> tm.add("A", i * i, new double[] {i}, new double[] {-i, -i * i})));
-//
-//    // Force to discard all versions
-//    monitoredApp.getLeft().getEpochManager().forceDiscardEpochs(__ -> true);
-//
-//    // perform GCs before exporting the store data
-//    performGC();
-//    final MemoryAnalysisService analysisService =
-//        (MemoryAnalysisService) createService(monitoredApp.getLeft(), monitoredApp.getRight());
-//    final Path exportPath = analysisService.exportMostRecentVersion("testOverview");
-//
-//    final IMemoryStatistic stats = loadMemoryStatFromFolder(exportPath);
-//    summary = MemoryStatisticsTestUtils.getStatisticsSummary(stats);
-//
-//    // Start a monitoring datastore with the exported data
-//    ManagerDescriptionConfig config = new ManagerDescriptionConfig();
-//    final IDatastore monitoringDatastore = StartBuilding.datastore()
-//        .setSchemaDescription(config.schemaDescription())
-//        .build();
-//
-//    // Start a monitoring cube
-//    IActivePivotManager manager = StartBuilding.manager()
-//        .setDescription(config.managerDescription())
-//        .setDatastoreAndPermissions(monitoringDatastore)
-//        .buildAndStart();
-//    monitoringApp = new Pair<>(monitoringDatastore, manager);
-//
-//    // Fill the monitoring datastore
-//    monitoringDatastore.edit(
-//        tm -> stats.accept(new FeedVisitor(monitoringDatastore.getSchemaMetadata(), tm, "storeA")));
-//
-//    IMultiVersionActivePivot pivot =
-//        monitoringApp.getRight().getActivePivots().get(ManagerDescriptionConfig.MONITORING_CUBE);
-//    Assertions.assertThat(pivot).isNotNull();
-//  }
-//
-//  @After
-//  public void tearDown() throws AgentException {
-//    monitoringApp.getLeft().close();
-//    monitoringApp.getRight().stop();
-//  }
-//
-//  @Test
-//  public void testVectorBlockRecordConsumptionIsZero() throws QueryException {
-//    final IMultiVersionActivePivot pivot =
-//        monitoringApp.getRight().getActivePivots().get(ManagerDescriptionConfig.MONITORING_CUBE);
-//
-//    final MDXQuery recordQuery = new MDXQuery(
-//        "SELECT [Components].[Component].[Component].[RECORDS] ON ROWS,"
-//            + " [Measures].[DirectMemory.SUM] ON COLUMNS"
-//            + " FROM [MemoryCube]"
-//            + " WHERE ("
-//            + "   [Owners].[Owner].[Owner].[Store A],"
-//            + "   [Fields].[Field].[Field].[vector1]"
-//            + " )");
-//
-//    final CellSetDTO result = pivot.execute(recordQuery);
-//
-//    Assertions.assertThat((long) result.getCells().get(0).getValue())
-//        .isZero();
-//  }
-//
-//  @Test
-//  public void testVectorBlockConsumption() throws QueryException {
-//    final IMultiVersionActivePivot pivot =
-//        monitoringApp.getRight().getActivePivots().get(ManagerDescriptionConfig.MONITORING_CUBE);
-//
-//    final MDXQuery vectorBlockQuery = new MDXQuery(
-//        "SELECT {"
-//            + "   [Components].[Component].[ALL].[AllMember],"
-//            + "   [Components].[Component].[Component].[VECTOR_BLOCK]"
-//            + " } ON ROWS,"
-//            + " [Measures].[DirectMemory.SUM] ON COLUMNS"
-//            + " FROM [MemoryCube]"
-//            + " WHERE ("
-//            + "   [Owners].[Owner].[Owner].[Store A],"
-//            + "   [Fields].[Field].[Field].[vector1]"
-//            + " )");
-//
-//    final CellSetDTO result = pivot.execute(vectorBlockQuery);
-//
-//    Assertions.assertThat((long) result.getCells().get(1).getValue())
-//        .isEqualTo((long) result.getCells().get(0).getValue());
-//  }
-//
-//  @Test
-//  public void testVectorBlockLength() throws QueryException {
-//    final IMultiVersionActivePivot pivot =
-//        monitoringApp.getRight().getActivePivots().get(ManagerDescriptionConfig.MONITORING_CUBE);
-//
-//    final MDXQuery lengthQuery = new MDXQuery(
-//        "SELECT  [Measures].[VectorBlock.Length] ON COLUMNS"
-//            + " FROM [MemoryCube]"
-//            + " WHERE ("
-//            + "   [Owners].[Owner].[Owner].[Store A],"
-//            + "   [Fields].[Field].[Field].[vector1]"
-//            + " )");
-//
-//    final CellSetDTO result = pivot.execute(lengthQuery);
-//
-//    Assertions.assertThat(CellSetUtils.extractValueFromSingleCellDTO(result))
-//        .isEqualTo((long) MICROAPP_VECTOR_BLOCK_SIZE);
-//  }
-//
-//  @Test
-//  public void testVectorBlockRefCount() throws QueryException {
-//    final IMultiVersionActivePivot pivot =
-//        monitoringApp.getRight().getActivePivots().get(ManagerDescriptionConfig.MONITORING_CUBE);
-//
-//    final MDXQuery refCountQuery = new MDXQuery(
-//        "SELECT [Measures].[VectorBlock.RefCount] ON COLUMNS"
-//            + " FROM [MemoryCube]"
-//            + " WHERE ("
-//            + "   [Owners].[Owner].[Owner].[Store A],"
-//            + "   [Fields].[Field].[Field].[vector1]"
-//            + " )");
-//
-//    final CellSetDTO refCountResult = pivot.execute(refCountQuery);
-//
-//    Assertions.assertThat((long) CellSetUtils.extractValueFromSingleCellDTO(refCountResult))
-//        .isEqualTo(FIELD_SHARING_COUNT * ADDED_DATA_SIZE);
-//  }
-//}
+/*
+ * (C) ActiveViam 2020
+ * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
+ * property of ActiveViam. Any unauthorized use,
+ * reproduction or transfer of this material is strictly prohibited
+ */
+
+package com.activeviam.mac.statistic.memory;
+
+import com.activeviam.copper.testing.CubeTester;
+import com.activeviam.mac.statistic.memory.descriptions.MicroApplicationDescriptionWithSharedVectorField;
+import com.activeviam.mac.statistic.memory.junit.RegistrySetupExtension;
+import com.activeviam.properties.impl.ActiveViamProperty;
+import com.activeviam.properties.impl.ActiveViamPropertyExtension;
+import com.activeviam.properties.impl.ActiveViamPropertyExtension.ActiveViamPropertyExtensionBuilder;
+import com.qfs.junit.LocalResourcesExtension;
+import com.qfs.monitoring.offheap.MemoryStatisticsTestUtils;
+import com.qfs.monitoring.offheap.MemoryStatisticsTestUtils.StatisticsSummary;
+import com.qfs.monitoring.statistic.memory.IMemoryStatistic;
+import com.qfs.util.impl.QfsFileTestUtils;
+import com.quartetfs.biz.pivot.IMultiVersionActivePivot;
+import com.quartetfs.biz.pivot.dto.CellSetDTO;
+import com.quartetfs.biz.pivot.query.impl.MDXQuery;
+import com.quartetfs.fwk.AgentException;
+import com.quartetfs.fwk.query.QueryException;
+import java.nio.file.Path;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+@ExtendWith(RegistrySetupExtension.class)
+public class TestVectorBlockBookmark extends ATestMemoryStatistic {
+
+	@RegisterExtension
+	protected static ActiveViamPropertyExtension propertyExtension =
+			new ActiveViamPropertyExtensionBuilder()
+					.withProperty(ActiveViamProperty.ACTIVEVIAM_TEST_PROPERTY, true)
+					.build();
+
+	@RegisterExtension
+	protected final LocalResourcesExtension resources = new LocalResourcesExtension();
+
+	protected static Path tempDir = QfsFileTestUtils.createTempDirectory(TestMACMeasures.class);
+
+	protected Application monitoredApplication;
+	protected Application monitoringApplication;
+	protected StatisticsSummary statisticsSummary;
+	protected CubeTester tester;
+
+	@BeforeEach
+	public void setup() throws AgentException {
+		monitoredApplication = MonitoringTestUtils.setupApplication(
+				new MicroApplicationDescriptionWithSharedVectorField(),
+				resources,
+				MicroApplicationDescriptionWithSharedVectorField::fillWithGenericData);
+
+		final Path exportPath = MonitoringTestUtils.exportMostRecentVersion(
+				monitoredApplication.getDatastore(),
+				monitoredApplication.getManager(),
+				tempDir,
+				this.getClass().getSimpleName());
+
+		final IMemoryStatistic stats = MonitoringTestUtils.loadMemoryStatFromFolder(exportPath);
+		statisticsSummary = MemoryStatisticsTestUtils.getStatisticsSummary(stats);
+
+		monitoringApplication = MonitoringTestUtils.setupMonitoringApplication(stats, resources);
+
+		tester = MonitoringTestUtils.createMonitoringCubeTester(monitoringApplication.getManager());
+	}
+
+	@Test
+	public void testVectorBlockRecordConsumptionIsZero() throws QueryException {
+		tester.mdxQuery("SELECT [Components].[Component].[Component].[RECORDS] ON ROWS,"
+				+ " [Measures].[DirectMemory.SUM] ON COLUMNS"
+				+ " FROM [MemoryCube]"
+				+ " WHERE ("
+				+ "   [Owners].[Owner].[Owner].[Store A],"
+				+ "   [Fields].[Field].[Field].[vector1]"
+				+ " )").getTester()
+				.hasOnlyOneCell()
+				.containing(0L);
+	}
+
+	@Test
+	public void testVectorBlockConsumption() throws QueryException {
+		final IMultiVersionActivePivot pivot = tester.pivot();
+
+		final MDXQuery vectorBlockQuery = new MDXQuery(
+				"SELECT {"
+						+ "   [Components].[Component].[ALL].[AllMember],"
+						+ "   [Components].[Component].[Component].[VECTOR_BLOCK]"
+						+ " } ON ROWS,"
+						+ " [Measures].[DirectMemory.SUM] ON COLUMNS"
+						+ " FROM [MemoryCube]"
+						+ " WHERE ("
+						+ "   [Owners].[Owner].[Owner].[Store A],"
+						+ "   [Fields].[Field].[Field].[vector1]"
+						+ " )");
+
+		final CellSetDTO result = pivot.execute(vectorBlockQuery);
+
+		Assertions.assertThat((long) result.getCells().get(1).getValue())
+				.isEqualTo((long) result.getCells().get(0).getValue());
+	}
+
+	@Test
+	public void testVectorBlockLength() {
+		tester.mdxQuery("SELECT  [Measures].[VectorBlock.Length] ON COLUMNS"
+				+ " FROM [MemoryCube]"
+				+ " WHERE ("
+				+ "   [Owners].[Owner].[Owner].[Store A],"
+				+ "   [Fields].[Field].[Field].[vector1]"
+				+ " )")
+				.getTester()
+				.hasOnlyOneCell()
+				.containing((long) MicroApplicationDescriptionWithSharedVectorField.VECTOR_BLOCK_SIZE);
+	}
+
+	@Test
+	public void testVectorBlockRefCount() {
+		tester.mdxQuery("SELECT [Measures].[VectorBlock.RefCount] ON COLUMNS"
+				+ " FROM [MemoryCube]"
+				+ " WHERE ("
+				+ "   [Owners].[Owner].[Owner].[Store A],"
+				+ "   [Fields].[Field].[Field].[vector1]"
+				+ " )")
+				.getTester()
+				.hasOnlyOneCell()
+				.containing((long) MicroApplicationDescriptionWithSharedVectorField.FIELD_SHARING_COUNT
+						* MicroApplicationDescriptionWithSharedVectorField.ADDED_DATA_SIZE);
+	}
+}
---
+++ src/test/java/com/activeviam/mac/statistic/memory/descriptions/MicroApplicationDescriptionWithSharedVectorField.java
@@ -0,0 +1,54 @@
+/*
+ * (C) ActiveViam 2020
+ * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
+ * property of ActiveViam. Any unauthorized use,
+ * reproduction or transfer of this material is strictly prohibited
+ */
+
+package com.activeviam.mac.statistic.memory.descriptions;
+
+import com.activeviam.pivot.builders.StartBuilding;
+import com.qfs.desc.IDatastoreSchemaDescription;
+import com.qfs.literal.ILiteralType;
+import com.qfs.store.IDatastore;
+import com.quartetfs.biz.pivot.IActivePivotManager;
+import com.quartetfs.biz.pivot.definitions.IActivePivotManagerDescription;
+import com.quartetfs.biz.pivot.definitions.impl.ActivePivotManagerDescription;
+import java.util.stream.IntStream;
+
+public class MicroApplicationDescriptionWithSharedVectorField
+		implements ITestApplicationDescription {
+
+	public static final int VECTOR_BLOCK_SIZE = 64;
+	public static final int CHUNK_SIZE = 256;
+	public static final int FIELD_SHARING_COUNT = 2;
+	public static final int ADDED_DATA_SIZE = 20;
+
+	@Override
+	public IDatastoreSchemaDescription datastoreDescription() {
+		return StartBuilding.datastoreSchema()
+				.withStore(
+						StartBuilding.store()
+								.withStoreName("A")
+								.withField("id", ILiteralType.INT)
+								.asKeyField()
+								.withVectorField("vector1", ILiteralType.DOUBLE)
+								.withVectorBlockSize(VECTOR_BLOCK_SIZE)
+								.withVectorField("vector2", ILiteralType.DOUBLE)
+								.withVectorBlockSize(VECTOR_BLOCK_SIZE)
+								.withChunkSize(CHUNK_SIZE)
+								.build())
+				.build();
+	}
+
+	@Override
+	public IActivePivotManagerDescription managerDescription(
+			IDatastoreSchemaDescription schemaDescription) {
+		return new ActivePivotManagerDescription();
+	}
+
+	public static void fillWithGenericData(IDatastore datastore, IActivePivotManager manager) {
+		datastore.edit(tm -> IntStream.range(0, ADDED_DATA_SIZE)
+				.forEach(i -> tm.add("A", i * i, new double[] {i}, new double[] {-i, -i * i})));
+	}
+}
