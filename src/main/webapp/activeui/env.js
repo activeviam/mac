a8022dfada649dd41bdfc59d65f1f76c6f158e70
Roxane Delpeyrat <54800346+roxanedelpeyratactiveviam@users.noreply.github.com>
2022-02-03 14:53:44 +0100

    Update src/main/webapp/activeui/env.js
    Co-authored-by: Mahieddine Dellabani <mde@activeviam.com>

--- src/main/webapp/activeui/env.js
+++ src/main/webapp/activeui/env.js
@@ -2,7 +2,7 @@ var baseUrl = window.location.href.split('/ui')[0];
 
 window.env = {
   contentServerUrl: baseUrl,
-  contentServerVersion: "5.11.1",
+  contentServerVersion: "5.11.x",
   // WARNING: Changing the keys of activePivotServers will break previously saved widgets and dashboards.
   // If you must do it, then you also need to update each one's serverKey attribute on your content server.
   activePivotServers: {
